09e081ed2d52c7414ebc07a2bf7c863f910d79e6
bronte999 <isabella.rahs@gmail.com>
2022-04-06 12:20:20 -0700

    Backend code

---
+++ backend.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const users = { 
+    users_list :
+    [
+       { 
+          id : 'xyz789',
+          name : 'Charlie',
+          job: 'Janitor',
+       },
+       {
+          id : 'abc123', 
+          name: 'Mac',
+          job: 'Bouncer',
+       },
+       {
+          id : 'ppp222', 
+          name: 'Mac',
+          job: 'Professor',
+       }, 
+       {
+          id: 'yat999', 
+          name: 'Dee',
+          job: 'Aspring actress',
+       },
+       {
+          id: 'zap555', 
+          name: 'Dennis',
+          job: 'Bartender',
+       }
+    ]
+ };
+
+app.use(express.json());
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        users['users_list'].splice(users['users_list'].indexOf(result),1);
+        res.send(users);
+    }
+});
+
+app.get('/users', (req, res) => {
+    const name = req.query.name; //or req.params.id
+    const job = req.query.job;
+    let result = findUserByNameAndJob(name, job);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
+})
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+function findUserByNameAndJob(name, job) { 
+    return users['users_list'].filter( (user) => user['name'] === name && user['job'] === job);
+}
+
+/*const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+}*/
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+
+function addUser(user){
+    users['users_list'].push(user);
+}
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});      
+
+
+
